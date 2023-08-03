667b45223f170e85277afd9dc1a02f47701ec940
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-08-03 08:31:24 -0700

    encodeing data

---
+++ Data Preprocessing-With my dataset.py
@@ -0,0 +1,46 @@
+import numpy
+import pandas
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+
+
+data = {
+    'Age': [35, 45, 28, 52, 30, 38, 29, 41, 33, 50, 36, 48, 31, 42, 27],
+    'Gender': ['Male', 'Female', 'Male', 'Male', 'Female', 'Female', 'Male', 'Male', 'Female', 'Female', 'Male', 'Female', 'Male', 'Male', 'Female'],
+    'Income': [50000, 60000, 42000, 80000, 55000, 75000, 48000, 70000, 52000, 90000, 58000, 82000, 49000, 72000, 43000],
+    'Education': ['Bachelor', 'Master', 'High School', 'PhD', 'Bachelor', 'Master', 'High School', 'Bachelor', 'Master', 'PhD', 'Bachelor', 'Master', 'High School', 'Bachelor', 'Master'],
+    'Marital_Status': ['Single', 'Married', 'Single', 'Married', 'Single', 'Married', 'Single', 'Married', 'Single', 'Married', 'Single', 'Married', 'Single', 'Married', 'Single']
+}
+# lable encoder is only apply on the singe colume of the data
+labelencodedata = LabelEncoder()
+gender = data['Gender']
+encodedata = labelencodedata.fit_transform(gender)
+print('label encoded data')
+print(encodedata)
+
+data_ = [
+    [35, 'Male', 50000, 'Bachelor', 'Single'],
+    [45, 'Female', 60000, 'Master', 'Married'],
+    [28, 'Male', 42000, 'High School', 'Single'],
+    [52, 'Male', 80000, 'PhD', 'Married'],
+    [30, 'Female', 55000, 'Bachelor', 'Single'],
+    [38, 'Female', 75000, 'Master', 'Married'],
+    [29, 'Male', 48000, 'High School', 'Single'],
+    [41, 'Male', 70000, 'Bachelor', 'Married'],
+    [33, 'Female', 52000, 'Master', 'Single'],
+    [50, 'Female', 90000, 'PhD', 'Married'],
+    [36, 'Male', 58000, 'Bachelor', 'Single'],
+    [48, 'Female', 82000, 'Master', 'Married'],
+    [31, 'Male', 49000, 'High School', 'Single'],
+    [42, 'Male', 72000, 'Bachelor', 'Married'],
+    [27, 'Female', 43000, 'Master', 'Single']
+]
+
+hotencoded = OneHotEncoder()
+hotdata = hotencoded.fit_transform(data_)
+arraydata = hotdata.toarray()
+
+print('Hot Encoded Data')
+
+print(arraydata)
--- Data Preprocessing.py
+++ Data Preprocessing.py
@@ -1,7 +1,44 @@
-import numpy as np 
-import pandas as pd  
-from sklearn import SimpleImputer
+from json import encoder
+import numpy as np
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
-data =  pd.read_csv('Churn_Modelling.csv')
-# x = data[['_unit_id', '']]
-print(data)  
+# Step 1: Load the data from 'Churn_Modelling.csv'
+data = pd.read_csv('Churn_Modelling.csv')
+
+
+# Step 2: Select the columns you want to impute (assuming you want to impute numerical columns)
+numerical_columns = ['CreditScore']
+# Step 3: Handle missing values in numerical columns using SimpleImputer with 'mean' strategy
+imputer = SimpleImputer(strategy='mean')
+newdata= imputer.fit_transform(data[numerical_columns])
+
+# Sample categorical data
+categories = ['cat', 'dog', 'bird', 'cat', 'dog']
+label_encoder = LabelEncoder()
+encoded_categories = label_encoder.fit_transform(categories) 
+print("Original Data:", categories)
+print("Encoded Data:", encoded_categories)
+
+print('Original Data:')
+print(data)
+
+
+
+
+# for the onhot encoder 
+
+categories_ = [['cat'], ['dog'], ['bird'], ['cat'], ['dog']]
+
+encoder_ = OneHotEncoder()
+
+
+encoder_Data = encoder_.fit_transform(categories_)
+encoded_array = encoder_Data.toarray()
+print('this is the hotencode data ') 
+print(encoded_array)
+
+
+
