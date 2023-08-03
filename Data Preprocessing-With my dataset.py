d4789fa3437cd20ea1abf727c38a2af1beec80c8
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-08-03 08:40:23 -0700

    Update Data Preprocessing-With my dataset.py

--- Data Preprocessing-With my dataset.py
+++ Data Preprocessing-With my dataset.py
@@ -3,7 +3,7 @@ import pandas
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
-
+from sklearn.model_selection import train_test_split
 
 data = {
     'Age': [35, 45, 28, 52, 30, 38, 29, 41, 33, 50, 36, 48, 31, 42, 27],
@@ -26,7 +26,7 @@ data_ = [
     [52, 'Male', 80000, 'PhD', 'Married'],
     [30, 'Female', 55000, 'Bachelor', 'Single'],
     [38, 'Female', 75000, 'Master', 'Married'],
-    [29, 'Male', 48000, 'High School', 'Single'],
+    [29, 'Male', 48000, 'High School', 'Single'], 
     [41, 'Male', 70000, 'Bachelor', 'Married'],
     [33, 'Female', 52000, 'Master', 'Single'],
     [50, 'Female', 90000, 'PhD', 'Married'],
@@ -44,3 +44,9 @@ arraydata = hotdata.toarray()
 print('Hot Encoded Data')
 
 print(arraydata)
+
+
+# train_test_split this is used to testing the model on the import data 
+
+
+
