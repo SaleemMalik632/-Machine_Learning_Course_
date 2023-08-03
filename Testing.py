b07eb9d6bbd94b8cf94d479e00d2f1fc384afb8c
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-08-03 00:48:34 -0700

    testing

---
+++ Testing.py
@@ -0,0 +1,4 @@
+a = 6
+b = 9
+print(a+b) 
+print('Dear Customer, your RAPID account application has been reviewed, however following discrepancy(ies) have been identified (1 )Fund Supporter source of income required ).You are requested to resume application using the link https://rapid.bankalfalah.com to regularize your account. For further details, contact us at  9221111225111 or contactusB!bankalfalah.com')
