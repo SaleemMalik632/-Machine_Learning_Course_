f315d1b4ed28cb2b76eca52e2ea4048de141a288
SaleemMalik632 <115183904+SaleemMalik632@users.noreply.github.com>
2023-08-05 23:18:15 -0700

    Update Language Detection.py

--- Language Detection.py
+++ Language Detection.py
@@ -1,10 +1,24 @@
-import pandas as pd 
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import accuracy_score, classification_report
 
 
-data  = pd.read_csv('Language Detection.csv')
+data = pd.read_csv('Language Detection.csv')
 
-df = pd.DataFrame(data)
-print(df) 
+x = data['Text']
+Label = data['Language']
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(x)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, Label)    
+
+Model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
+Model.fit(X, Label) 
+text= 'жаль что ты со школы так изменилась, пьешь много?'
+TestSimple = vectorizer.transform([text]) 
+print(Model.score(X_test , y_test)) 
+TestSimple_pred = Model.predict(TestSimple)
+print(TestSimple_pred)
